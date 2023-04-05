c2bc1b0e24910a98dcadbff3936b2bdc55f1e755
neimar79 <Nima87760@gmail.com>
2023-04-05 17:56:05 +0430

    initial commit

---
+++ codlearnprojectbot/credentials.py
@@ -0,0 +1,3 @@
+# Do not remove these 2 lines:
+BOT_TOKEN = '5865336962:AAGivavrqXrzplZ-9cydSeixh_XP4fiaGHA'  # You should consider using env variables or a secret manager for this.
+APP_NAME = 'codlearnprojectbot'
